6d833a44b18f33f22bdc96fc7e097ab6c80fbfc3
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-04 20:16:48 +0200

    wrap the login image with shaderMask

--- lib/Features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/Features/auth/presentation/views/widgets/login_view_body.dart
@@ -9,11 +9,25 @@ class LoginViewBody extends StatelessWidget {
     return Stack(
       alignment: Alignment.topCenter,
       children: [
-        Image.asset(
-          'assets/images/loginImage.png',
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height * 0.6,
-          fit: BoxFit.cover,
+        ShaderMask(
+          shaderCallback: (bounds) {
+            return const LinearGradient(
+                colors: [
+                  Colors.black,
+                  Colors.grey,
+                  Colors.white,
+                  Colors.black,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0, .1, .5, 1]).createShader(bounds);
+          },
+          child: Image.asset(
+            'assets/images/loginImage.png',
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.6,
+            fit: BoxFit.cover,
+          ),
         ),
         Column(
           children: [
