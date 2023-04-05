ce8f28d2550b8bb3fa918fb8801eab3b3a50e9d1
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-05 23:06:48 +0200

    configure flutterfire

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:photoplay/constants.dart';
 import 'package:photoplay/core/utils/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const PhotoPlay());
 }
 
