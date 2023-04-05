55ea4d4d42356df7196f8a843fda1c26b9207ddd
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-05 03:33:17 +0200

    create default button

---
+++ lib/core/widgets/default_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:photoplay/constants.dart';
+import 'package:photoplay/core/utils/styles.dart';
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton(
+      {super.key, required this.onPressed, required this.btnText});
+  final void Function() onPressed;
+  final String btnText;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 44,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kPrimatyColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          btnText,
+          style: Styles.text14b.copyWith(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
