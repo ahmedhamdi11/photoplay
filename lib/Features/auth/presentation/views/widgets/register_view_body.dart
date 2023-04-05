7c51c0cc789b05f3ebe8f49e8c9c4518a74e42c7
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-06 01:53:41 +0200

    textformfield validation done

--- lib/Features/auth/presentation/views/widgets/register_view_body.dart
+++ lib/Features/auth/presentation/views/widgets/register_view_body.dart
@@ -8,9 +8,16 @@ import 'package:photoplay/core/widgets/custom_text_feild.dart';
 import 'package:photoplay/core/widgets/default_back_btn.dart';
 import 'package:photoplay/core/widgets/default_button.dart';
 
-class RegisterViewBody extends StatelessWidget {
+class RegisterViewBody extends StatefulWidget {
   const RegisterViewBody({super.key});
 
+  @override
+  State<RegisterViewBody> createState() => _RegisterViewBodyState();
+}
+
+class _RegisterViewBodyState extends State<RegisterViewBody> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   @override
   Widget build(BuildContext context) {
     RegisterCubit registerCubit = BlocProvider.of<RegisterCubit>(context);
@@ -53,102 +60,151 @@ class RegisterViewBody extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(horizontal: 45.0),
                   child: SingleChildScrollView(
                     physics: const BouncingScrollPhysics(),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        //Register TextFields
-                        Text(
-                          'FIRST NAME',
-                          style: Styles.text12b,
-                        ),
-                        const SizedBox(
-                          height: 8.0,
-                        ),
-                        CustomTextField(
-                          hint: 'first name here',
-                          onChanged: (value) {
-                            registerCubit.firstName = value;
-                          },
-                        ),
-                        const SizedBox(
-                          height: 25.0,
-                        ),
-                        Text(
-                          'LAST NAME',
-                          style: Styles.text12b,
-                        ),
-                        const SizedBox(
-                          height: 8.0,
-                        ),
-                        CustomTextField(
-                          hint: 'last name here',
-                          onChanged: (value) {
-                            registerCubit.lastName = value;
-                          },
-                        ),
-                        const SizedBox(
-                          height: 25.0,
-                        ),
-                        Text(
-                          'EMAIL',
-                          style: Styles.text12b,
-                        ),
-                        const SizedBox(
-                          height: 8.0,
-                        ),
-                        CustomTextField(
-                          hint: 'email here',
-                          onChanged: (value) {
-                            registerCubit.email = value;
-                          },
-                        ),
-                        const SizedBox(
-                          height: 25.0,
-                        ),
-                        Text(
-                          'PASSWORD',
-                          style: Styles.text12b,
-                        ),
-                        const SizedBox(
-                          height: 8.0,
-                        ),
-                        CustomTextField(
-                          hint: 'password here',
-                          onChanged: (value) {
-                            registerCubit.password = value;
-                          },
-                        ),
-                        const SizedBox(
-                          height: 25.0,
-                        ),
-                        Text(
-                          'CONFIRM PASSWORD',
-                          style: Styles.text12b,
-                        ),
-                        const SizedBox(
-                          height: 8.0,
-                        ),
-                        const CustomTextField(
-                          hint: 'confirm password here',
-                        ),
-                        const SizedBox(
-                          height: 21.0,
-                        ),
+                    child: Form(
+                      key: formKey,
+                      autovalidateMode: autovalidateMode,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //Register TextFields
+                          Text(
+                            'FIRST NAME',
+                            style: Styles.text12b,
+                          ),
+                          const SizedBox(
+                            height: 8.0,
+                          ),
+                          CustomTextField(
+                            hint: 'first name here',
+                            onChanged: (value) {
+                              registerCubit.firstName = value;
+                            },
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'please inter your first name';
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                          const SizedBox(
+                            height: 25.0,
+                          ),
+                          Text(
+                            'LAST NAME',
+                            style: Styles.text12b,
+                          ),
+                          const SizedBox(
+                            height: 8.0,
+                          ),
+                          CustomTextField(
+                            hint: 'last name here',
+                            onChanged: (value) {
+                              registerCubit.lastName = value;
+                            },
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'please inter your last name';
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                          const SizedBox(
+                            height: 25.0,
+                          ),
+                          Text(
+                            'EMAIL',
+                            style: Styles.text12b,
+                          ),
+                          const SizedBox(
+                            height: 8.0,
+                          ),
+                          CustomTextField(
+                            hint: 'email here',
+                            onChanged: (value) {
+                              registerCubit.email = value;
+                            },
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'email cannot be empty';
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                          const SizedBox(
+                            height: 25.0,
+                          ),
+                          Text(
+                            'PASSWORD',
+                            style: Styles.text12b,
+                          ),
+                          const SizedBox(
+                            height: 8.0,
+                          ),
+                          CustomTextField(
+                            hint: 'password here',
+                            onChanged: (value) {
+                              registerCubit.password = value;
+                            },
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'password cannot be empty';
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                          const SizedBox(
+                            height: 25.0,
+                          ),
+                          Text(
+                            'CONFIRM PASSWORD',
+                            style: Styles.text12b,
+                          ),
+                          const SizedBox(
+                            height: 8.0,
+                          ),
+                          CustomTextField(
+                            hint: 'confirm password here',
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'confirm your password';
+                              } else if (value != registerCubit.password) {
+                                return 'enter the same password';
+                              }
+                            },
+                          ),
+                          const SizedBox(
+                            height: 21.0,
+                          ),
 
-                        //Register button
-                        state is RegisterLoadingState
-                            ? const Center(child: CircularProgressIndicator())
-                            : DefaultButton(
-                                onPressed: () {
-                                  BlocProvider.of<RegisterCubit>(context)
-                                      .registerUser(
-                                    email: registerCubit.email!,
-                                    password: registerCubit.password!,
-                                  );
-                                },
-                                btnText: 'REGISTER',
-                              )
-                      ],
+                          //Register button
+                          state is RegisterLoadingState
+                              ? const Center(child: CircularProgressIndicator())
+                              : DefaultButton(
+                                  onPressed: () {
+                                    if (formKey.currentState!.validate()) {
+                                      BlocProvider.of<RegisterCubit>(context)
+                                          .registerUser(
+                                        email: registerCubit.email!,
+                                        password: registerCubit.password!,
+                                      );
+                                    } else {
+                                      setState(() {
+                                        autovalidateMode =
+                                            AutovalidateMode.always;
+                                      });
+                                    }
+                                  },
+                                  btnText: 'REGISTER',
+                                ),
+                          const SizedBox(
+                            height: 21.0,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
--- lib/core/widgets/custom_text_feild.dart
+++ lib/core/widgets/custom_text_feild.dart
@@ -3,32 +3,34 @@ import 'package:photoplay/constants.dart';
 import 'package:photoplay/core/utils/styles.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, this.suffix, this.hint, this.onChanged});
+  const CustomTextField(
+      {super.key, this.suffix, this.hint, this.onChanged, this.validator});
   final Widget? suffix;
   final String? hint;
   final void Function(String)? onChanged;
+  final String? Function(String?)? validator;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 42,
-      child: TextField(
-        onChanged: onChanged,
-        cursorColor: kPrimatyColor,
-        decoration: InputDecoration(
-            contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
-            fillColor: const Color(0xff212121),
-            filled: true,
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: BorderSide.none),
-            suffixIcon: Padding(
-              padding: const EdgeInsets.only(right: 20),
-              child: suffix,
-            ),
-            suffixIconConstraints: const BoxConstraints(maxHeight: 42),
-            hintText: hint,
-            hintStyle: Styles.text17),
-      ),
+    return TextFormField(
+      validator: validator,
+      onChanged: onChanged,
+      cursorColor: kPrimatyColor,
+      decoration: InputDecoration(
+          isDense: true,
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 20.0, vertical: 13.0),
+          fillColor: const Color(0xff212121),
+          filled: true,
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: BorderSide.none),
+          suffixIcon: Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: suffix,
+          ),
+          suffixIconConstraints: const BoxConstraints(maxHeight: 42),
+          hintText: hint,
+          hintStyle: Styles.text17),
     );
   }
 }
