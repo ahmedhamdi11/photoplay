a404fe32cc546ac20dd7e0b60361dd6703d6ae5c
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-05 20:21:11 +0200

    edit app logo

--- lib/Features/auth/presentation/views/widgets/app_logo.dart
+++ lib/Features/auth/presentation/views/widgets/app_logo.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:photoplay/core/utils/styles.dart';
 
-class LoginLogo extends StatelessWidget {
-  const LoginLogo({
+class AppLogo extends StatelessWidget {
+  const AppLogo({
     super.key,
   });
 
@@ -11,9 +11,6 @@ class LoginLogo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.2,
-        ),
         SvgPicture.asset('assets/images/logo.svg'),
         const SizedBox(
           height: 10.0,
