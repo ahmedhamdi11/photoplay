0d1105c9bef991f88ca5a6717842020031105b88
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-05 22:53:56 +0200

    create auth repo

---
+++ lib/Features/auth/data/auth_repo.dart
@@ -0,0 +1,5 @@
+abstract class AuthRepo {
+  loginUser();
+  registerUser();
+  resetPassword();
+}
---
+++ lib/Features/auth/data/auth_repo_impl.dart
@@ -0,0 +1,21 @@
+import 'package:photoplay/Features/auth/data/auth_repo.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  @override
+  loginUser() {
+    // TODO: implement loginUser
+    throw UnimplementedError();
+  }
+
+  @override
+  registerUser() {
+    // TODO: implement registerUser
+    throw UnimplementedError();
+  }
+
+  @override
+  resetPassword() {
+    // TODO: implement resetPassword
+    throw UnimplementedError();
+  }
+}
