ea51ff9140abc340ee7b3b8106c3de7cacceca17
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-06 07:11:52 +0200

    form validate on login view

--- lib/Features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/Features/auth/presentation/views/widgets/login_view_body.dart
@@ -10,12 +10,21 @@ import 'package:photoplay/core/utils/styles.dart';
 import 'package:photoplay/core/widgets/custom_text_feild.dart';
 import 'package:photoplay/core/widgets/default_button.dart';
 
-class LoginViewBody extends StatelessWidget {
+class LoginViewBody extends StatefulWidget {
   const LoginViewBody({super.key, required this.state});
   final LoginStates state;
+
+  @override
+  State<LoginViewBody> createState() => _LoginViewBodyState();
+}
+
+class _LoginViewBodyState extends State<LoginViewBody> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   @override
   Widget build(BuildContext context) {
     LoginCubit loginCubit = BlocProvider.of<LoginCubit>(context);
+
     return Stack(
       children: [
         //background image
@@ -26,114 +35,137 @@ class LoginViewBody extends StatelessWidget {
           physics: const BouncingScrollPhysics(),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 45.0),
-            child: Column(
-              children: [
-                //logo
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.2,
-                ),
-                const AppLogo(),
-                const SizedBox(
-                  height: 50,
-                ),
-
-                //email field
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'EMAIL',
-                    style: Styles.text12b,
+            child: Form(
+              key: formKey,
+              autovalidateMode: autovalidateMode,
+              child: Column(
+                children: [
+                  //logo
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
                   ),
-                ),
-                const SizedBox(
-                  height: 8.0,
-                ),
-                CustomTextField(
-                  hint: 'email here',
-                  onChanged: (value) {
-                    loginCubit.email = value;
-                  },
-                ),
-                const SizedBox(
-                  height: 25.0,
-                ),
-
-                //password field
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'PASSWORD',
-                    style: Styles.text12b,
+                  const AppLogo(),
+                  const SizedBox(
+                    height: 50,
                   ),
-                ),
-                const SizedBox(
-                  height: 8.0,
-                ),
-                CustomTextField(
-                  hint: 'password here',
-                  onChanged: (value) {
-                    loginCubit.password = value;
-                  },
-                  suffix: TextButton(
-                    onPressed: () {
-                      GoRouter.of(context)
-                          .push(AppRouter.resetPasswordViewPath);
-                    },
+
+                  //email field
+                  Align(
+                    alignment: Alignment.centerLeft,
                     child: Text(
-                      'FORGOT?',
-                      style: Styles.text11,
+                      'EMAIL',
+                      style: Styles.text12b,
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 21.0,
-                ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  CustomTextField(
+                    hint: 'email here',
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'email cannot be empty';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      loginCubit.email = value;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 25.0,
+                  ),
 
-                //login button
-                state is LoginLoadingState
-                    ? const Center(
-                        child: CircularProgressIndicator(),
-                      )
-                    : DefaultButton(
-                        onPressed: () {
-                          loginCubit.loginUser();
-                        },
-                        btnText: 'LOGIN',
+                  //password field
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'PASSWORD',
+                      style: Styles.text12b,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  CustomTextField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'password cannot be empty';
+                      } else {
+                        return null;
+                      }
+                    },
+                    hint: 'password here',
+                    onChanged: (value) {
+                      loginCubit.password = value;
+                    },
+                    suffix: TextButton(
+                      onPressed: () {
+                        GoRouter.of(context)
+                            .push(AppRouter.resetPasswordViewPath);
+                      },
+                      child: Text(
+                        'FORGOT?',
+                        style: Styles.text11,
                       ),
-                const SizedBox(
-                  height: 29.0,
-                ),
-
-                //login with facebook and googl
-                const SocialLogins(),
-                const SizedBox(
-                  height: 42.0,
-                ),
-
-                //register button
-                Text(
-                  'Don\'t have an account?',
-                  style: Styles.text14m.copyWith(
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white.withAlpha(70),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 8.0,
-                ),
-                InkWell(
-                  onTap: () {
-                    GoRouter.of(context).push(AppRouter.registerViewPath);
-                  },
-                  child: Text(
-                    'REGISTER',
-                    style: Styles.text14b,
+                  const SizedBox(
+                    height: 21.0,
                   ),
-                ),
-                const SizedBox(
-                  height: 40.0,
-                ),
-              ],
+
+                  //login button
+                  widget.state is LoginLoadingState
+                      ? const Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : DefaultButton(
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              loginCubit.loginUser();
+                            } else {
+                              autovalidateMode = AutovalidateMode.always;
+                              setState(() {});
+                            }
+                          },
+                          btnText: 'LOGIN',
+                        ),
+                  const SizedBox(
+                    height: 29.0,
+                  ),
+
+                  //login with facebook and googl
+                  const SocialLogins(),
+                  const SizedBox(
+                    height: 42.0,
+                  ),
+
+                  //register button
+                  Text(
+                    'Don\'t have an account?',
+                    style: Styles.text14m.copyWith(
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white.withAlpha(70),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      GoRouter.of(context).push(AppRouter.registerViewPath);
+                    },
+                    child: Text(
+                      'REGISTER',
+                      style: Styles.text14b,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40.0,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
