b0360657ac85a8ab6b6bebaa1c0bd9649caf95f1
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-26 19:04:50 +0200

    movie model created

---
+++ lib/Features/home/data/models/movie_model.dart
@@ -0,0 +1,43 @@
+import 'package:equatable/equatable.dart';
+
+class MovieModel extends Equatable {
+  final int id;
+  final String title;
+  final String overview;
+  final String posterPath;
+  final dynamic voteAverage;
+  final int voteCount;
+  final List genreIds;
+
+  const MovieModel({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.posterPath,
+    required this.voteAverage,
+    required this.voteCount,
+    required this.genreIds,
+  });
+
+  factory MovieModel.fromJson(jsonData) {
+    return MovieModel(
+      id: jsonData['id'],
+      title: jsonData['title'],
+      overview: jsonData['overview'],
+      posterPath: jsonData['poster_path'],
+      voteAverage: jsonData['vote_average'],
+      voteCount: jsonData['vote_count'],
+      genreIds: jsonData['genre_ids'],
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        overview,
+        posterPath,
+        voteAverage,
+        voteCount,
+      ];
+}
