c5f597ce71f9c12e4eba9b49d72e89095249af74
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-29 20:48:36 +0300

    bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:photoplay/constants.dart';
 import 'package:photoplay/core/utils/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:photoplay/core/utils/cash_helper.dart';
 import 'firebase_options.dart';
+import 'observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +14,7 @@ void main() async {
   );
   await CashHelper.init();
   CashHelper.uId = CashHelper.prefs.getString('uId');
+  Bloc.observer = MyBlocObserver();
   runApp(const PhotoPlay());
 }
 
