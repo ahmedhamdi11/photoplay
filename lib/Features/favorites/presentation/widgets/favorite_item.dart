e8148bf86e32e213531a74724a08451b854a320d
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-05-04 19:32:55 +0300

    favorites ui done

--- lib/Features/favorites/presentation/widgets/favorite_item.dart
+++ lib/Features/favorites/presentation/widgets/favorite_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:photoplay/core/utils/styles.dart';
 
@@ -13,32 +14,57 @@ class FavoriteItem extends StatelessWidget {
       child: Row(
         children: [
           SizedBox(
-            width: MediaQuery.of(context).size.width * 0.41,
+            height: MediaQuery.of(context).size.height * 0.18,
             child: AspectRatio(
-              aspectRatio: 3 / 1.7,
-              child: Image.asset(
-                'assets/images/test_image2.png',
-                fit: BoxFit.cover,
+              aspectRatio: 2 / 3,
+              child: CachedNetworkImage(
+                imageUrl:
+                    'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89058/93685/Joker-2019-Final-Style-steps-Poster-buy-original-movie-posters-at-starstills__62518.1669120603.jpg?c=2', // imageUrl: '$kImageBaseUrl/w300${movie.posterPath}',
+                placeholder: (context, url) =>
+                    const Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
               ),
             ),
           ),
-          const SizedBox(width: 10.0),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Narcos',
-                style: Styles.text15m,
-              ),
-              const SizedBox(
-                height: 4.0,
-              ),
-              Text(
-                '4 Episodes | 5.02GB',
-                style:
-                    Styles.text12b.copyWith(color: Colors.white.withAlpha(67)),
-              ),
-            ],
+          const SizedBox(width: 12.0),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      child: Text(
+                        'Joker',
+                        style: Styles.text15m,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 4.0,
+                    ),
+                    Text(
+                      '(media type)',
+                      style: Styles.text11.copyWith(
+                          color: Colors.grey, fontWeight: FontWeight.normal),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 4.0,
+                ),
+                Text(
+                  'overview ;alskdjfa;lskdfja;lsdkfja;sdklfja;sdklfja;sdklfja;sdklfja;sdklfja;sdklfja;sdlkf',
+                  style: Styles.text12b
+                      .copyWith(color: Colors.white.withAlpha(67)),
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
           )
         ],
       ),
