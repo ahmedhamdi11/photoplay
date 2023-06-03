393f51f0d9f783c1d1812dcd047045b9edb0a191
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-06-03 13:07:17 +0300

    fix logo and circle avatar in the light theme

--- lib/Features/auth/presentation/views/widgets/app_logo.dart
+++ lib/Features/auth/presentation/views/widgets/app_logo.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:photoplay/constants.dart';
+import 'package:photoplay/core/cubits/theme_cubit/theme_cubit.dart';
 import 'package:photoplay/core/utils/styles.dart';
 
 class AppLogo extends StatelessWidget {
@@ -9,17 +12,44 @@ class AppLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SvgPicture.asset('assets/images/logo.svg'),
-        const SizedBox(
-          height: 10.0,
-        ),
-        Text(
-          'PHOTOPLAY',
-          style: Styles.text16,
-        ),
-      ],
-    );
+    bool isDarkTheme = BlocProvider.of<ThemeCubit>(context).isDarkTheme;
+    return isDarkTheme
+        ? Column(
+            children: [
+              SvgPicture.asset('assets/images/logo.svg'),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'PHOTOPLAY',
+                style: Styles.text16,
+              ),
+            ],
+          )
+        : Container(
+            padding: const EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+              color: kLightScaffoldColor,
+              borderRadius: BorderRadius.circular(16.0),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 16.0,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                SvgPicture.asset('assets/images/logo.svg'),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'PHOTOPLAY',
+                  style: Styles.text16,
+                ),
+              ],
+            ),
+          );
   }
 }
--- lib/Features/auth/presentation/views/widgets/register_add_picture.dart
+++ lib/Features/auth/presentation/views/widgets/register_add_picture.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:photoplay/Features/auth/presentation/manager/cubits/register_cubit/register_cubit.dart';
+import 'package:photoplay/core/cubits/theme_cubit/theme_cubit.dart';
 import 'package:photoplay/core/utils/styles.dart';
 
 class RegisterAddPicture extends StatefulWidget {
@@ -21,20 +22,51 @@ class _RegisterAddPictureState extends State<RegisterAddPicture> {
 
   @override
   Widget build(BuildContext context) {
+    bool isDarkTheme = BlocProvider.of<ThemeCubit>(context).isDarkTheme;
+
     ImagePicker picker = ImagePicker();
     return Column(
       children: [
-        CircleAvatar(
-          radius: 50,
-          backgroundColor: const Color(0xff1D1D1D),
-          backgroundImage: imageFile == null ? null : FileImage(imageFile!),
-          child: imageFile != null
-              ? null
-              : SvgPicture.asset('assets/images/person.svg'),
-        ),
+        isDarkTheme
+            ?
+            // circle avatar whit no shadows
+            CircleAvatar(
+                radius: 50,
+                backgroundColor: const Color(0xff1D1D1D),
+                backgroundImage:
+                    imageFile == null ? null : FileImage(imageFile!),
+                child: imageFile != null
+                    ? null
+                    : SvgPicture.asset('assets/images/person.svg'),
+              )
+            :
+            // circle avatar with shadows for the light theme
+            Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.35),
+                        blurRadius: 16.0,
+                        spreadRadius: 3)
+                  ],
+                ),
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.white,
+                  backgroundImage:
+                      imageFile == null ? null : FileImage(imageFile!),
+                  child: imageFile != null
+                      ? null
+                      : SvgPicture.asset('assets/images/person.svg'),
+                ),
+              ),
+
         const SizedBox(
           height: 16.0,
         ),
+
+        // upload image button
         TextButton(
           onPressed: () async {
             XFile? xFile = await picker.pickImage(source: ImageSource.gallery);
